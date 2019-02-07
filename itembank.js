e51aa672503072c5c4b2a370d2c6b80c414e1633
Jonathan Angus <jangusff@gmail.com>
2019-02-06 18:22:20 -0600

    commit for updated itembank.js

--- itembank.js
+++ itembank.js
@@ -1,123 +1,123 @@
-const ITEMBANK = [
-    {
-      question: 'Which video game was the first to feature a first-person perspective?',
-      answers: [
-        'Scramble',
-        'BattleZone',
-        'Lode Runner',
-        'Star Raiders',
-        ],
-      correctAnswer: 'BattleZone',
-      displayImg: 'images/Battlezone.jpg',
-      alt: 'battle zone video game image'
-    },
-    {
-      question: 'What was the first video game to allow players to input initials into a High Score table?',
-      answers: [
-        'Out Run',
-        'Missle Command',
-        'Dragontorc',
-        'Asteroids',
-        ],
-      correctAnswer: 'Asteroids',
-      displayImg: 'images/Asteroids.jpg',
-      alt: 'asteroids video game image'
-    },
-    {
-      question: 'Which of these titles is considered to be the first real blockbuster video game?',
-      answers: [
-        'Pong',
-        'Space Invaders',
-        'Pac Man',
-        'Breakout',
-        ],
-      correctAnswer: 'Space Invaders',
-      displayImg: 'images/SpaceInvaders.jpg',
-      alt: 'space invaders video game image'
-    },
-    {
-      question: 'Which was the first video game to feature side-scrolling?',
-      answers: [
-        'Sinistar',
-        'Robotron 2084',
-        'Space Panic',
-        'Defender',
-        ],
-      correctAnswer: 'Defender',
-      displayImg: 'images/Defender.jpg',
-      alt: 'defender video game image'
-    },
-    {
-      question: 'The game that launched the Nintendo rocket was?',
-      answers: [
-        'Donkey Kong',
-        'Mario Bros.',
-        'Moon Patrol',
-        'Dig Dug',
-        ],
-      correctAnswer: 'Donkey Kong',
-      displayImg: 'images/DonkeyKong.jpg',
-      alt: 'donkey kong video game image'
-    },
-    {
-      question: 'What was the first video game that included multiple levels?',
-      answers: [
-        'Tank Battle',
-        'Mr. Do',
-        'Gorf',
-        'Super Bagman',
-        ],
-      correctAnswer: 'Gorf',
-      displayImg: 'images/Gorf.jpg',
-      alt: 'gorf video game image'
-    },
-    {
-      question: 'This was the first game to star a female character:',
-      answers: [
-        'Lady Bug',
-        'Ms. Pac Man',
-        'Pooyan',
-        'Millipede',
-        ],
-      correctAnswer: 'Ms. Pac Man',
-      displayImg: 'images/MsPacMan.jpg',
-      alt: 'ms pac man video game image'
-    },
-    {
-      question: 'Atari\'s first color vector game was?',
-      answers: [
-        'Tempest',
-        'Phoenix',
-        'Qix',
-        'Joust',
-        ],
-      correctAnswer: 'Tempest',
-      displayImg: 'images/Tempest.jpg',
-      alt: 'tempest video game image'
-    },
-    {
-      question: 'This was the first wildly successful driving game:',
-      answers: [
-        'Rally X',
-        'Turbo',
-        'Pole Position',
-        'RC Pro-Am',
-        ],
-      correctAnswer: 'Pole Position',
-      displayImg: 'images/PolePosition.jpg',
-      alt: 'pole position video game image'
-    },
-    { 
-      question: 'What was the first video game to have a movie tie-in?',
-      answers: [
-        'Star Wars',
-        'Sky Kid',
-        'Galaxian',
-        'Tron',
-        ],
-      correctAnswer: 'Tron',
-      displayImg: 'images/Tron.jpg',
-      alt: 'tron position video game image'
-    },
+let ITEMBANK = [
+  {
+    question: 'Which video game was the first to feature a first-person perspective?',
+    answers: [
+      'Scramble',
+      'BattleZone',
+      'Lode Runner',
+      'Star Raiders',
+      ],
+    correctAnswer: 'BattleZone',
+    displayImg: 'images/Battlezone.jpg',
+    alt: 'battle zone video game image'
+  },
+  {
+    question: 'What was the first video game to allow players to input initials into a \'High Score\' table?',
+    answers: [
+      'Out Run',
+      'Missle Command',
+      'Dragontorc',
+      'Asteroids',
+      ],
+    correctAnswer: 'Asteroids',
+    displayImg: 'images/Asteroids.jpg',
+    alt: 'asteroids video game image'
+  },
+  {
+    question: 'Which of these titles is considered to be the first real blockbuster video game?',
+    answers: [
+      'Pong',
+      'Space Invaders',
+      'Pac Man',
+      'Breakout',
+      ],
+    correctAnswer: 'Space Invaders',
+    displayImg: 'images/SpaceInvaders.jpg',
+    alt: 'space invaders video game image'
+  },
+  {
+    question: 'Which was the first video game to feature side-scrolling?',
+    answers: [
+      'Sinistar',
+      'Robotron 2084',
+      'Space Panic',
+      'Defender',
+      ],
+    correctAnswer: 'Defender',
+    displayImg: 'images/Defender.jpg',
+    alt: 'defender video game image'
+  },
+  {
+    question: 'The game that launched the Nintendo rocket was?',
+    answers: [
+      'Donkey Kong',
+      'Mario Bros.',
+      'Moon Patrol',
+      'Dig Dug',
+      ],
+    correctAnswer: 'Donkey Kong',
+    displayImg: 'images/DonkeyKong.jpg',
+    alt: 'donkey kong video game image'
+  },
+  {
+    question: 'What was the first video game that included multiple levels?',
+    answers: [
+      'Tank Battle',
+      'Mr. Do',
+      'Gorf',
+      'Super Bagman',
+      ],
+    correctAnswer: 'Gorf',
+    displayImg: 'images/Gorf.jpg',
+    alt: 'gorf video game image'
+  },
+  {
+    question: 'This was the first game to star a female character:',
+    answers: [
+      'Lady Bug',
+      'Ms. Pac Man',
+      'Pooyan',
+      'Millipede',
+      ],
+    correctAnswer: 'Ms. Pac Man',
+    displayImg: 'images/MsPacMan.jpg',
+    alt: 'ms pac man video game image'
+  },
+  {
+    question: 'Atari\'s first color vector game was?',
+    answers: [
+      'Tempest',
+      'Phoenix',
+      'Qix',
+      'Joust',
+      ],
+    correctAnswer: 'Tempest',
+    displayImg: 'images/Tempest.jpg',
+    alt: 'tempest video game image'
+  },
+  {
+    question: 'This was the first wildly successful driving game:',
+    answers: [
+      'Rally X',
+      'Turbo',
+      'Pole Position',
+      'RC Pro-Am',
+      ],
+    correctAnswer: 'Pole Position',
+    displayImg: 'images/PolePosition.jpg',
+    alt: 'pole position video game image'
+  },
+  { 
+    question: 'What was the first video game to have a movie tie-in?',
+    answers: [
+      'Star Wars',
+      'Sky Kid',
+      'Galaxian',
+      'Tron',
+      ],
+    correctAnswer: 'Tron',
+    displayImg: 'images/Tron.jpg',
+    alt: 'tron position video game image'
+  },
 
-  ];
+];
